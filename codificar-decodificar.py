71a78befbd9f31ba2348f2513d6aaace2dfbb54e
Enolla Cristine Bressanim <49999110+enollabressanim@users.noreply.github.com>
2020-08-17 22:23:23 -0300

    Update codificar-decodificar.py

--- codificar-decodificar.py
+++ codificar-decodificar.py
@@ -4,17 +4,17 @@ a = input(str("digite C para codificar ou D para decodificar um texo: "))
 
 def codificar():
     if a == "C":
-        message = input("digite o texto para codificar: ")
-        message_bytes = message.encode('ascii')
-        base64_bytes = base64.b64encode(message_bytes)
+        texto = input("digite o texto para codificar: ")
+        texto_bytes = texto.encode('ascii')
+        base64_bytes = base64.b64encode(texto_bytes)
         base64_message = base64_bytes.decode('ascii')
         print(base64_message)
 codificar()
 
 def decodificar():
     if a == "D":
-        encodedStr = input("digite o texto para decodificar: ")
-        decodedBytes = base64.b64decode(encodedStr)
-        decodedStr = str(decodedBytes, "utf-8")
-        print(decodedStr)
+        codigo = input("digite o texto para decodificar: ")
+        decodificarBytes = base64.b64decode(codigo)
+        decodificarStr = str(decodificarBytes, "utf-8")
+        print(decodificarStr)
 decodificar()
